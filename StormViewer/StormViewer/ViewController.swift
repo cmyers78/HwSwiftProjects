693d742d60d6a22e18c6c9de55cb90db9ae62a54
Chris Myers <cmyers78@gmail.com>
2016-12-27 15:48:41 -0500

    Initial Commit of StormViewer

---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Christopher Myers on 12/27/16.
+//  Copyright © 2016 Dragoman Developers, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
