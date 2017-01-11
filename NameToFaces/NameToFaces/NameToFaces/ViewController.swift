31af8478281cdbad79fe182a7dff15d8c6492e04
Chris Myers <cmyers78@gmail.com>
2017-01-11 10:54:31 -0500

    had to re-embed nav controller...disappeared for some reason.

--- NameToFaces/NameToFaces/NameToFaces/ViewController.swift
+++ NameToFaces/NameToFaces/NameToFaces/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     override func viewDidLoad() {
         super.viewDidLoad()
         print("setup")
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -27,17 +27,17 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
         
-//        let person = people[indexPath.item]
-//        
-//        cell.name.text  = person.name
-//        
-//        let path = getDocumentsDirectory().appendingPathComponent(person.image)
-//        cell.imageView.image = UIImage(contentsOfFile: path.path)
-//        
-//        cell.imageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
-//        cell.imageView.layer.borderWidth = 2
-//        cell.imageView.layer.cornerRadius = 3
-//        cell.layer.cornerRadius = 7
+        let person = people[indexPath.item]
+        
+        cell.name.text  = person.name
+        
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        
+        cell.imageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
         
         return cell
     }
